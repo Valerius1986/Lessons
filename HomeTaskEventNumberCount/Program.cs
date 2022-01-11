176a8a817e65e18d80e34ea109b888dabbec1e9d
Валерий Борисенко <valcom1986@gmail.com>
2022-01-11 13:10:57 +0300

    Create evenOddCount

--- HomeTaskEventNumberCount/Program.cs
+++ HomeTaskEventNumberCount/Program.cs
@@ -10,7 +10,31 @@ namespace HomeTaskEventNumberCount
     {
         static void Main(string[] args)
         {
+            int count =0;
+            int evenCount = 0;
+            int oddCount = 0;
 
+            Console.WriteLine("Введите диапазон чисел в котором будет подсчитывать количество четных чисел: ");
+            int limit = int.Parse(Console.ReadLine());
+
+            while (count < limit)
+            {
+                count++;
+                if (count % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+                
+            }
+
+            Console.Clear();
+            Console.WriteLine("В диапазоне чисел от 0 до " + limit + ".");
+            Console.WriteLine("Чётных чисел: " + evenCount);
+            Console.WriteLine("Нечётных чисел: " + oddCount);
         }
     }
 }
